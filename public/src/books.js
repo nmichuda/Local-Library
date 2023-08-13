c4560e00540034b04eaf6b67ba3b89ec969111ba
nmichuda <nmichuda@gmail.com>
2023-08-13 12:29:28 -0500

    more comments

--- public/src/books.js
+++ public/src/books.js
@@ -1,5 +1,5 @@
 //const { findAccountById } = require("./accounts.js");
-function findAccountById(accounts, id) {
+function findAccountById(accounts, id) { //could not get the function to import correctly. unsure whats going on. copy pasted function over and works fine.
   for(let i=0;i<accounts.length;i++){
     if(accounts[i].id===id){
       return accounts[i];
@@ -10,7 +10,7 @@ function findAccountById(accounts, id) {
   
 }
 
-function findAuthorById(authors, id) {
+function findAuthorById(authors, id) { //returns the author that has matching id. 
   for(let i=0;i<authors.length;i++){
     if(authors[i].id === id){
       return authors[i]
@@ -19,7 +19,7 @@ function findAuthorById(authors, id) {
   return null;
 }
 
-function findBookById(books, id) {
+function findBookById(books, id) { //iterates through books. if id matches, returns the book objet.
   for(let i=0;i<books.length;i++){
     if(books[i].id === id){
       return books[i]
@@ -29,31 +29,31 @@ function findBookById(books, id) {
 }
 
 function partitionBooksByBorrowedStatus(books) {
-  let checkedOut = [];
-  let notCheckedOut = [];
-  for(let i =0;i<books.length;i++){
-    if(books[i].borrows.some((book)=>book.returned===false)){
+  let checkedOut = []; //create an array for checked out books
+  let notCheckedOut = []; //create an array for not checked out books
+  for(let i =0;i<books.length;i++){ //iterate through all the books.
+    if(books[i].borrows.some((book)=>book.returned===false)){ //push all books not returned into checked out array
       checkedOut.push(books[i]);
     }
-    else{
+    else{ //push all other books into notchecked out array
       notCheckedOut.push(books[i]);
     }
 
   }
-  let results = [checkedOut,notCheckedOut];
+  let results = [checkedOut,notCheckedOut]; //results array set to checked out books array and not checked out array.
   //console.log(results);
   return results;
 }
 
 function getBorrowersForBook(book, accounts) {
-  let results = [];
+  let results = []; //create results array
  // console.log(findAccountById(accounts,book.borrows[1].id));
   
-  for(let i =0; i<book.borrows.length && i<10;i++){
-    let foundAccount = findAccountById(accounts,book.borrows[i].id);
+  for(let i =0; i<book.borrows.length && i<10;i++){ //iterate thorugh books array up to 10.
+    let foundAccount = findAccountById(accounts,book.borrows[i].id); //identify account by id
     //console.log(foundAccount);
-    foundAccount["returned"] = book.borrows[i].returned;
-    results.push(foundAccount);
+    foundAccount["returned"] = book.borrows[i].returned; //push returned status to returned key
+    results.push(foundAccount); //push found account to results
     //console.log(results);
   }
   //console.log(results);
